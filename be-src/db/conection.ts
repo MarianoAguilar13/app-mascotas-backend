71dc2e5c6c4f9fd62fc6d53f710efa842201d3c4
Mariano <mariano_aguilar_13@hotmail.com>
2023-01-31 17:00:01 -0300

    Arreglando los datos de la base de datos para que pueda hacerse correctamente el deploy

--- be-src/db/conection.ts
+++ be-src/db/conection.ts
@@ -1,14 +1,11 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-export const sequelize = new Sequelize(process.env.POSTGRE_ELEPHANT_URL);
-/*
 export const sequelize = new Sequelize({
   dialect: "postgres",
   username: process.env.POSTGRE_USERNAME,
   password: process.env.POSTGRE_PASSWORD,
   database: process.env.POSTGRE_DATABASE,
-  port: 5432,
   host: process.env.POSTGRE_HOST,
   ssl: true,
   // esto es necesario para que corra correctamente
@@ -18,7 +15,7 @@ export const sequelize = new Sequelize({
       rejectUnauthorized: false,
     },
   },
-});*/
+});
 
 async function main() {
   try {
